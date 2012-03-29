562e3fa705a54b8e68853aad7d7dfa24ac9b02d1
Greg Allen <me@jga.me>
2012-03-29 13:31:02 -0700

    initial commit

---
+++ bin/stalk.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var watch = require('../lib/watch');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+
+var list = function(val) {
+  return val.split(',');
+};
+
+program
+  .version(JSON.parse(fs.readFileSync(__dirname + '/../package.json', 'utf8')).version)
+  .option('-p, --path <path>', 'Path to watch [defaults to cwd]', process.cwd())
+  .option('-i, --ignore <path>', 'Paths to ignore [path1,path2]', list)
+  .usage('[cmd]')
+  .parse(process.argv);
+
+if (program.args.length !== 0) {
+  watch(program.path, { ignore: program.ignore || [] }, function() {
+    exec(program.args, function(error, stdout, stderr) {
+      if (error)
+        throw error;
+      if (stdout)
+        console.log(stdout);
+      if (stderr)
+        console.log(stderr);
+    });
+  });
+} else {
+  process.stdout.write(program.helpInformation());
+  program.emit('--help');
+  process.exit(1);
+}
